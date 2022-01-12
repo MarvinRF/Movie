8fa53596d4af0e280029d9d2d5e85c4b64078b26
Marvin Rocha <mamavivi@gmail.com>
2022-01-12 19:55:42 -0300

    Security config

--- frontend/src/Components/Navbar/index.tsx
+++ frontend/src/Components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as GithubIcon } from 'assets/img/Github.svg';
 import './styles.css'
+
 function Navbar() {
 
   return (
@@ -7,7 +8,8 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-Content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/MarvinRF">
+          <a href="https://github.com/MarvinRF ">
+          
             <div className="dsmovie-contact-container">
               <GithubIcon />
               <p className="dsmovie-contact-link">/MarvinRF</p>
