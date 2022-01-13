e86ba318f4b9e5e471f0108bd4150655c71c886d
Marvin Rocha <mamavivi@gmail.com>
2022-01-13 19:11:32 -0300

    ajuste navbar

--- frontend/src/Components/Navbar/index.tsx
+++ frontend/src/Components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <header>
       <nav className="container">
-        <div className="dsmovie-nav-Content">
+        <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
           <a href="https://github.com/MarvinRF ">          
             <div className="dsmovie-contact-container">
