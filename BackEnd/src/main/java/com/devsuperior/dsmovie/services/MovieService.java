9dfb7dbe6524a279b4d72943d81bf4a6b9a87a5e
Marvin Rocha <mamavivi@gmail.com>
2022-01-13 03:07:51 -0300

    First homolog

--- BackEnd/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ BackEnd/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -10,8 +10,11 @@ import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.repositories.MovieRepository;
 
+
+
 @Service
 public class MovieService {
+
 	
 	@Autowired
 	private MovieRepository repository;
