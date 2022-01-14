575d04baeb79af078701193e254f35fa77c5abdb
Marvin Rocha <mamavivi@gmail.com>
2022-01-14 13:03:24 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,28 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
+
 
 function Listing() {
 
+const [pageNumber, setPagenumber] = useState(0);
+useEffect(() =>{
     axios.get(`${BASE_URL}/movies?size=12&page=0`)
     .then(response => {
-    console.log(response.data);
+    const data = response.data as MoviePage;
+    setPagenumber(data.number);
     });
+}, []);
+    
+
 
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="Container">
